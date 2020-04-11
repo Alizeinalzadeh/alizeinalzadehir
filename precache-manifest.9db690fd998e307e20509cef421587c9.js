470d015fab93ad5dfc6155d90b27a96111cbdbe0
Ali <alizeinalzadeh@yahoo.com>
2020-04-11 22:25:31 +0430

    Updates

---
+++ precache-manifest.9db690fd998e307e20509cef421587c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da54a11bf9cc77de2c85bcd9a0e2b8c0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "450550349a9658e2bcec",
+    "url": "./static/css/main.5c8db792.chunk.css"
+  },
+  {
+    "revision": "cc699927f738cb6a53f4",
+    "url": "./static/js/2.b3fbb1f4.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "./static/js/2.b3fbb1f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "450550349a9658e2bcec",
+    "url": "./static/js/main.31d22b00.chunk.js"
+  },
+  {
+    "revision": "c43aa25c9c309835920b",
+    "url": "./static/js/runtime-main.d00fe099.js"
+  }
+]);
